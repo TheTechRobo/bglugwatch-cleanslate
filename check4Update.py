c30d1544805dd2f202e55778ba043015f9377087
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-05-04 12:03:36 -0400

    ADD ERROR HANDLING

--- check4Update.py
+++ check4Update.py
@@ -2,13 +2,15 @@
 import requests
 
 url = 'https://thetechrobo.github.io/resources/bglug.py' #url to DL
+try:
+        r = requests.get(url, stream = True) #DL the URL
 
-r = requests.get(url, stream = True) #DL the URL
+        with open("bglug.py", "wb") as Pypdf:
+  
+                for chunk in r.iter_content(chunk_size = 1024):
 
-with open("bglug.py", "wb") as Pypdf:
-
-	for chunk in r.iter_content(chunk_size = 1024):
-
-		if chunk:
-
-			Pypdf.write(chunk) #write 1KB(?) at a time
+		        if chunk:
+  
+     			        Pypdf.write(chunk) #write 1KB(?) at a time
+except:
+	print("There was an ERROR upgrading to the LATEST VERSION. You may have outdated information")
