ce982932a4ce2bdaf97d20229650c163dac7643e
TtR <52163910+TheTechRobo@users.noreply.github.com>
2020-08-03 18:58:49 -0400

    IT WORKS\!

--- check4Update.py
+++ check4Update.py
@@ -12,9 +12,8 @@ try:
 """
 url = 'https://thetechrobo.github.io/resources/bglug.py' #url to DL
 try: 
-    r = requests.get(url) #download
-    r = r.read().decode("UTF-8") #change from binary to unicode
-    print(r)
-    #exec(r) #run the code
-except Exception as ename:
+    r = requests.get(url).text #download
+    #r = r.decode("UTF-8") #change from binary to unicode
+    exec(r) #run the code
+except ImportError as ename:
 	print("There was an ERROR upgrading to the LATEST VERSION (%s). You may have outdated information." % ename)
